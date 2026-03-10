fed955cdf3ac8b99ae43f579216aec82db49b5e2
leesm9840 <leesm9840@gmail.com>
2026-03-10 13:10:03 +0900

    Updated Changes

--- week01/test.py
+++ week01/test.py
@@ -48,11 +48,13 @@ print(a[7:-11])
 a = "Python"
 print(a.count('p'))
 
-# 문자열
+# 문자 위치 확인
 print(a.find('y'))
 print(a.find('p'))
 print(a.index('y'))
+# print(a.index('p')) # 오류 발생
 
+# 문자 삽입
 b= ","
 c= b.join('Abcd')
 print(c)
