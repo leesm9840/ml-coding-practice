2505f24325f580dd2c95efde36798b2e798d3f2b
leesm9840 <leesm9840@gmail.com>
2026-03-10 12:50:02 +0900

    Updated Changes

--- week01/test.py
+++ week01/test.py
@@ -267,9 +267,15 @@ print(chr(48))
 print(ord('a'))
 print(ord('0'))
 
+print(dir([1, 2, 3]))
+print(dir({'1':'a'}))
+print(dir(1))
 
+print(int('3'))
+print(str(3))
 
-
+print(tuple("Python"))
+print(tuple([1, 2, 3]))
 
 
 
