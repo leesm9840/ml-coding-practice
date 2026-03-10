1fd36a97afcac6a9ade1f8c2b0463570e5e5e614
leesm9840 <leesm9840@gmail.com>
2026-03-10 11:46:44 +0900

    Updated Changes

--- week01/test.py
+++ week01/test.py
@@ -72,3 +72,16 @@ print(b.split(','))
 a = [1, 2, 3]
 b = ['Life', 'is', 'too', 'short']
 c = [1, 2, 'Life', 'is']
+d = [1, 2, [3, 4], ['Life', 'is']]
+
+print(d[0])
+print(d[2])
+print(d[3][-1])
+
+print(d[0:3])
+
+print(a+b)
+print(b[0] + " hi~ ^^;")
+
+print(a * 3)
+
