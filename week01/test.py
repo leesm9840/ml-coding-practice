209c967dff9fb5890f1c4437ccc9ec65b0628e67
leesm9840 <leesm9840@gmail.com>
2026-03-10 13:21:33 +0900

    Updated Changes

--- week01/test.py
+++ week01/test.py
@@ -226,10 +226,14 @@ s3 = set([4, 5, 6, 7, 8, 9])
 print(s3)
 
 # 교집합 연산
+print(s2 & s3)
+print(s2.intersection(s3))
+
+# 합집합 연산
 print(s2 | s3)
 print(s2.union(s3))
 
-# 합집합 연산
+
 print(s2 - s3)
 print(s3 - s2)
 print(s2.difference(s3))
