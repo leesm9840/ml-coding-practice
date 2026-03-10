666b8869ee082d0ec6a292a8a61ee68f358f05ca
leesm9840 <leesm9840@gmail.com>
2026-03-10 11:36:14 +0900

    Updated Changes

--- week01/test.py
+++ week01/test.py
@@ -13,4 +13,15 @@ print(7 // 3)
 s1 = 'Hello Python'
 print(s1)
 
-s3 = ''
+s3 = '''Hello
+Python'''
+print(s3)
+
+head = "Python"
+tail = "is fun"
+print(head + tail)
+
+print(head * 2)
+print("=" * 5)
+
+a=
