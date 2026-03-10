d9ccfaf12549beccc0562a445e825dea119d2473
leesm9840 <leesm9840@gmail.com>
2026-03-10 13:18:33 +0900

    Updated Changes

--- week01/test.py
+++ week01/test.py
@@ -173,29 +173,38 @@ t4[0:3]
 print(t1 + t2)
 # print(t1 + "hi~ ^^;")     #오류 발생
 
-# 튜플 연결
+# 튜플 반복
 t2 * 10
 
+# 튜플 수정(불가능)
+# t2[2] = 99        #오류 발생
+
+#딕셔너리 만들기
 dic = {'name':'Hong', 'phone':'01012345678', 'birth':'0814'}
 
+# 원소 추가
 dic[1] = 'a'
 print(dic)
 
 dic['pet'] = 'dog'
 print(dic)
 
+# 원소 삭제
 del dic[1]
 print(dic)
 
+# 원소의 value 구하기
 print(dic['phone'])
 print(dic['name'])
 
+# key의 리스트 만들기
 print(dic.keys())
 print(list(dic.keys()))
 
 for key in dic.keys():
     print(dic[key])
 
+# 
 print(dic.values())
 print(list(dic.values()))
 
