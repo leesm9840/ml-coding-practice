67201d288484631f0489072ed48458d111e4c7eb
leesm9840 <leesm9840@gmail.com>
2026-03-10 13:33:33 +0900

    Updated Changes

--- week01/test.py
+++ week01/test.py
@@ -352,6 +352,10 @@ print(int('3'))             # x를 정수 형태로 반환
 print(str(3))               # x를 문자열 형태로 반환
 
 # x를 리스트로 반환
+print(list("Python"))
+print((1, 2, 3))
+
+# x를 튜플로 반환
 print(tuple("Python"))
 print(tuple([1, 2, 3]))
 
