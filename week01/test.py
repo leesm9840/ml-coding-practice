766ad3b16c713932b7c9ba99416cc832845629f1
leesm9840 <leesm9840@gmail.com>
2026-03-10 12:12:14 +0900

    Updated Changes

--- week01/test.py
+++ week01/test.py
@@ -227,6 +227,17 @@ for score in [90, 25, 67, 45, 93]:
     number += 1
 
     if score > 60:
-        print("%d번 학생은")
+        print("%d번 학생은 합격입니다." %number)
+    else:
+        print("%d번 학생은 불합격입니다."%number)
+
+i = 0
+while i < 5:
+    i += 1
+    print('*' * i)
+
+def sum1(a, b):
+    x = 0
+    for i in args:
 
 
