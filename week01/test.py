32d94e2d6dd93e7dfeea42d120e9c1a20c9ab750
leesm9840 <leesm9840@gmail.com>
2026-03-10 12:46:02 +0900

    Updated Changes

--- week01/test.py
+++ week01/test.py
@@ -244,7 +244,14 @@ def sum2(*args):
     x = 0
     for i in args:
         x += i
-   return x
+    return x
+
+a = 5
+b = 3
+print(sum1(a, b))
+print(sum1(3, 5))
+print(sum2(1,2, 3, 4, 5))
+print(sum2(2, 3.5, 10))
 
 
 
