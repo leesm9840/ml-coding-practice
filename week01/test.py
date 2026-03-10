a0531361b94c86886ddc925089d63bdf81131cf0
leesm9840 <leesm9840@gmail.com>
2026-03-10 12:08:14 +0900

    Updated Changes

--- week01/test.py
+++ week01/test.py
@@ -209,7 +209,15 @@ if money >= 1200 and money < 3500:
 print(1 in [1, 2, 3])
 print(x in [1, 2, 3])
 print(x not in [1, 2, 3])
-print('a' in ['a', 'b', 'c', ]'d')
+print('a' in ['a', 'b', 'c', 'd'])
+print('i' not in 'Python')
+
+if money >= 10:
+    pass
+else:
+    print('저금하자!')
+
+test_list
 
 
 
