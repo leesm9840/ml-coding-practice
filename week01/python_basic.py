77b9f49a97d3f1f5b4a48af41ae83891fb09033d
leesm9840 <leesm9840@gmail.com>
2026-03-14 18:53:27 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -69,16 +69,16 @@ print(d.lstrip())
 print(d.rstrip())
 print(d.strip())
 
-# 문자?�� ?��?�� (불�???��)
+# 문자열 바꾸기
 a = "Python"
-# a[1] = 'y'    #?���? 발생
+# a[1] = 'y'    # 오류 발생
 
-# 문자?�� 바꾸�?
+# 문자열 바꾸기
 a = "Python is difficult."
 print(a.replace("difficult", "funny"))
 print(a)
 
-# 문자?�� ?��?���?
+# 문자열 나누기
 print(a.split())
 
 b = "a, b, c, d"
