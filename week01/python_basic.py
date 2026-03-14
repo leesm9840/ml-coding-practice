e51e298210b989ae1ae6aa5c53c6c73ab4a985ae
leesm9840 <leesm9840@gmail.com>
2026-03-14 17:02:24 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -345,6 +345,18 @@ print(ord('0'))
 
 # 객체 x가 가진 멤버 변수와 멤버 함수 보여주기
 print(dir([1, 2, 3]))
-print(dir(['1':'a']))
+print(dir({'1':'a'}))
+print(dir(1))
 
+print(int('3'))         # x를 정수 형태로 반환
+print(str(3))           # x를 문자열 형태로 반환
 
+# x를 리스트로 반환
+print(list("Python"))
+print((1, 2, 3))
+
+# x를 튜플로 반환
+print(tuple("Python"))
+print(tuple([1, 2, 3]))
+
+# x를 자료향을 반환
