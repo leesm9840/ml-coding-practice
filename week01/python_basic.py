57ae2c2487d50e0a5642df318f9c4236dd2086e6
leesm9840 <leesm9840@gmail.com>
2026-03-14 16:08:24 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -23,4 +23,26 @@ print(s3)
 head = "Python"
 tail = " is fun"
 print(head + tail)
+
 # 문자열 곱하기
+print(head * 2)
+print("=" * 5)
+
+# 문자열 인덱싱
+a = "Now is better than never"
+print(a[0])
+print(a[4])
+print(a[-1])
+print(a[-2])
+
+# 문자열 슬라이싱
+b = a[0] + a[1] + a[2]
+print(b)
+
+print(a[4:6])
+print(a[19:])
+print(a[:3])
+print(a[7:-11])
+
+# 문자 개수 계산
+a = "Python"
