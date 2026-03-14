fd0ccbd7006e96eb0c8b56e57da88df0a064e6c9
leesm9840 <leesm9840@gmail.com>
2026-03-14 19:08:28 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -427,10 +427,10 @@ for i in range(1, 6):
     f.write(data)
 f.close()
 
-# ?��?��?�� ?��?�� 추�???���?
+# 파일에 내용 추가하기
 f = open('example.txt', 'w')
 for i in range(6, 11):
-    data = '%d번째 �? 추�???��?��?��. \n' % i
+    data = '%d번째 줄 추가입니다. \n' % i
     f.write(data)
 f.close()
 
