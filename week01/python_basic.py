078f95fc75327ba05c5fc601685643b8e3152115
leesm9840 <leesm9840@gmail.com>
2026-03-14 17:23:54 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -455,3 +455,13 @@ for line in lines:
 
 f.close()
 
+# 파일 모드 'r' - read()
+f = open('example.txt', 'r')
+data = f.read()
+f.close()
+data
+
+# with open() as 파일 객체
+with open('example.txt', 'w') as f:
+    f.write("Now is better than never.")
+# data = f.read()       # 오류 발생
