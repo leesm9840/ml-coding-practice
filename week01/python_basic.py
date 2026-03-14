1fed4cbd67d07883aa92c62edc4ca3d689af93c3
leesm9840 <leesm9840@gmail.com>
2026-03-14 16:10:24 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -46,4 +46,10 @@ print(a[7:-11])
 
 # 문자 개수 계산
 a = "Python"
-print(a.count{'p'})
+print(a.count('p'))
+
+# 문자 위치 확인
+print(a.find('y'))
+print(a.find('p'))
+print(a.index('y'))
+# print(a.index('p')) # 오류 발생
