4185b5a3e6ab40e33aff43426f513295e5b5b2e0
leesm9840 <leesm9840@gmail.com>
2026-03-14 16:37:54 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -212,4 +212,17 @@ print(list(dic.values()))
 print(dic.items())
 
 for key, value in dic.items():
-    
+    print(key + ":" + value)
+
+# 원소 삭제
+dic.clear()
+print(dic)
+
+# 집합 만들기
+s1 = {1, 2, 'a', 5}
+s2 = set([1, 2, 3, 4, 5, 6])
+print(s2)
+s3 = set([4, 5, 6, 7, 8, 9])
+print(s3)
+
+#교집합 연산
