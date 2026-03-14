20a2d79b042ac37d155a8593890d76a92e0d2a97
leesm9840 <leesm9840@gmail.com>
2026-03-14 17:04:54 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -359,4 +359,17 @@ print((1, 2, 3))
 print(tuple("Python"))
 print(tuple([1, 2, 3]))
 
-# x를 자료향을 반환
+# x를 자료형을 반환
+print(type("abc"))
+print(type(a))
+
+# 간단한 삽입형 함수 생성
+sum = lambda a, b: a + b
+
+print(sum(3, 5))
+
+# 최대, 최소값 반환
+print(max([1, 4, 2, 8, 6]))
+print(max("Python"))
+
+print(min([1, 4, 2, 8, 6]))
