61d5f333c5f6918210df7fefaf40c17e3c48514e
leesm9840 <leesm9840@gmail.com>
2026-03-22 19:07:57 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -18,7 +18,7 @@ pd.DataFrame([[1,2,],[3,4],[5,6],[7,8]])
 pd.DataFrame([[1,2],[3,4],[5,6],[7,8]], columns = ['var_1','var_2'], index=['a','b','c','d'])
 
 import pandas as pd
-file_url = 'https://media/githubusercontect.com/media/musthave-ML10/data_source/main/sample_df.csv'
+file_url = 'https://media.githubusercontect.com/media/musthave-ML10/data_source/main/sample_df.csv'
 
 sample_df = pd.read_csv(file_url, index_col=0)
 print(sample_df.head())
@@ -97,7 +97,7 @@ print(sample_df.describe())
 print(sample_df.std())
 print(sample_df.agg(['count','mean', 'std', 'min', 'max']))
 
-file_url = 'https://media/githubusercontect.com/media/musthave-ML10/data_source/main/iris.csv'
+file_url = 'https://media.githubusercontect.com/media/musthave-ML10/data_source/main/iris.csv'
 iris = pd.read_csv(file_url)
 
 print(iris.head())
