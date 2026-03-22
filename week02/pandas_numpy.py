58de3add791260975083ce1bc045a9ff965b037a
leesm9840 <leesm9840@gmail.com>
2026-03-22 16:43:54 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -37,4 +37,6 @@ print(sample_df.loc['a':'c'])
 print(sample_df.iloc[[0,1,2]])
 print(sample_df.iloc[0:2])
 print(sample_df.iloc[0:3])
-print(sample_df.iloc[0:3, 2:4])         #
+print(sample_df.iloc[0:3, 2:4])        # 컬럼까지 동시에 인덱싱
+
+print(sa)
