dc3b9280246cca51bbb19d1cbd0a13a2f2fc6c08
leesm9840 <leesm9840@gmail.com>
2026-03-26 15:05:07 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -47,4 +47,7 @@ plt.xlabel('Embarked Port')
 plt.ylabel('Count')
 plt.xticks(survived_count.index, ['Southampton', 'Cherbourg', 'Queenstown'])
 plt.legend(['Survived'], loc='upper tight')
-plt.grid()
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+# 생존자 수 표시
+for i, value
