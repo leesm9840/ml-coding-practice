42cd27e032e449e20564dbf9de52271988c31b4c
leesm9840 <leesm9840@gmail.com>
2026-03-26 16:19:16 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -122,3 +122,11 @@ print(titanic.info(), '\n')
 # 나이 결측치 처리 후
 titanic = titanic.dropna(subset=['Age'])
 print(titanic.info())
+
+# 히스토그램 그리기
+plt.figure(figsize=(10, 6))
+plt.hist(titanic['Age'], bins=20, color='seagreen', edgecolor='black')
+
+plt.xlabel('Age')
+plt.ylabel('Count')
+plt.title('Distribution of Ages on the Titanic')
