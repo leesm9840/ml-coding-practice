fa988eced992df1e634c37d4b75184096ffca715
leesm9840 <leesm9840@gmail.com>
2026-03-26 16:20:16 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -138,3 +138,9 @@ plt.close()
 
 # 결측치 처리
 titanic = titanic.dropna(subset=['Age', 'Fare'])
+
+# 상관 행렬 계산
+correlation_matrix = titanic.drop('PassengerId', axis=1).corr(numeric_only=True)
+print(correlation_matrix)
+
+# 히트맵 그리기
