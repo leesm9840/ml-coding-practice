59ce60268e728c58668eec88d1e34f1be9e0bd7f
leesm9840 <leesm9840@gmail.com>
2026-03-26 15:03:07 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -42,4 +42,8 @@ print(survived_count)
 # 막대 그래프 그리기
 plt.bar(survived_count.index, survived_count,
         color = ['mediumorchid', 'darkviolet', 'indigo'])
-plt.title('Survivedm Counts by Embarked Port')
+plt.title('Survivedm Counts by Embarked Port on Titanic')
+plt.xlabel('Embarked Port')
+plt.ylabel('Count')
+plt.xticks(survived_count.index, ['Southampton', 'Cherbourg', 'Queenstown'])
+plt.legend('Survived')
