f5611d1d7f2d61184b8cd4412d07213cd79f74db
leesm9840 <leesm9840@gmail.com>
2026-03-26 15:19:07 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -85,4 +85,10 @@ print(titanic.info(), '\n')
 
 # 결측치 처리
 titanic = titanic.dropna(subset=['Age', 'Fare', 'Survived'])
-print(titanic.)
+print(titanic.info())
+
+# 산점도 그래프 그리기
+plt.figure(figsize=(12, 8))
+scatter = plt.scatter(x='Age', y='Fare', data=titanic, c=titanic['Survived'],cmap='Set2', alpha=0.7)
+
+plt.title('Age')
