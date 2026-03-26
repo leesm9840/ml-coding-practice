28dd157f68f0ada0da0d7e9fb3163683c5173647
leesm9840 <leesm9840@gmail.com>
2026-03-26 12:05:50 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -3,4 +3,17 @@
 import pandas as pd
 
 # 타이타닉 CSV 파일 불러오기
-titanic = pd.read.csv('3.1.1.titanic.csv')
+titanic = pd.read_csv('3.1.1.titanic.csv')
+
+# head( ) 함수를 출력하여 타이타닉 데이터셋의 구성을 간단히 살펴보기
+
+# 데이터 처음 5개의 행 출력
+print(titanic.head())
+
+# 열에 대한 요약 정보 확인
+print(titanic.info())
+
+"""### **선 그래프 : 객실 등급에 따른 생존율 표시하기**"""
+
+# 객실 등급에 따른 생존자와 사망자의 평균 계산
+pclass_survived_mean = titanic.groupby('Pclass')[Sury]
