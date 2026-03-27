5a3e6782b6fa0869a46e7d8e6fcafa5473b9ef81
leesm9840 <leesm9840@gmail.com>
2026-03-27 21:00:15 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -382,3 +382,8 @@ ax1.plot(x, y1, '-s', color='indigo', markersize=7, linewidth=5, alpha=0.7, labe
 ax1.set_xlabel('Parch') # x축의 레이블
 ax1.set_ylabel('Not Survived Count', color='indigo') # y축의 레이블
 ax1.tick_params(axis='y', labelcolor='indigo') # 눈금의 모양과 속성
+ax1.legend(loc='upper right', bbox_to_anchor=(1, 1)) # 범례
+
+# .twinx() 함수를 사용하여 두 번째 그래프와 축을 공유하는 새로운 Axes 객체 생성
+ax2 = ax1.twinx()
+
