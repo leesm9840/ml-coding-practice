13e5bfd18c3f4ac6bd59f5edbdbb2de0898e1236
leesm9840 <leesm9840@gmail.com>
2026-03-31 13:11:13 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -39,4 +39,4 @@ for set_ in (strat_test_set, strat_test_set):
 """
 
 housing = strat_train_set.drop("median_house_value", axis=1)
-housing_labels = strat_train_set[""]
+housing_labels = strat_train_set["median_house_valie"]
