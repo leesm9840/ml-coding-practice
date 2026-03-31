5c7d681ff926f3db684fc3c67d4b56ab48fddc55
leesm9840 <leesm9840@gmail.com>
2026-03-31 13:18:43 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -60,3 +60,8 @@ print(imputer.statistics_)           # imputer 결과 값
 print(housing_num.median().values)   # 수동으로 계산한 중간값
 
 # 훈현 세트의 누락값을 imputer가 학습한 값으로 채우기
+X = imputer.transform(housing_num)
+
+imputer.feature_names_in_
+
+housing_tr = pd.DataFrame(X, columns=housing_num.columns)
