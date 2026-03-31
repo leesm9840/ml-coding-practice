81ccb9e6fab059c17811590dad464aa2e2c857d7
leesm9840 <leesm9840@gmail.com>
2026-03-31 10:03:15 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -14,4 +14,5 @@ def load_housing_data():
         url = "https://github.com/ageron/data/raw/main/housing.tgz"
         urllib.request.urlretrieve(url, tarball_path)
         with tarfile.open(tarball_path) as housing_tarball:
+            housing_tarball.extractall(path="darasets")
             
