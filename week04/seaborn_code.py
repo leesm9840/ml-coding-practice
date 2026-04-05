d7078fb5985dc6cf2355df59f06903412806cdbc
leesm9840 <leesm9840@gmail.com>
2026-04-05 22:11:19 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -32,3 +32,8 @@ plt.savefig('./week04/Seaborn_Figure01.jpg')
 # **빈도 그래프**
 # figure에 2개의 서브 플롯을 생성
 fig = plt.figure(figsize=(15, 5))
+ax1 = fig.add_subplot(1, 2 ,1)
+ax2 = fig.add_subplot(1, 2, 2)
+
+# 식사가 이루어진 시간대 파악
+# x측 변수, 데이터셋, axe 객체(1번째 그리기
