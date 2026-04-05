1a6dc24907864da512eb0d134efc1f0705fe548a
leesm9840 <leesm9840@gmail.com>
2026-04-05 22:25:49 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -76,4 +76,8 @@ plt.savefig('./week04/Seaborn_Figure05.jpg')
 sns.jointplot(x='size', y='tip', data=tips, kind='scatter')
 plt.savefig('./week04/Seaborn_Figure05.jpg')
 
-# 
+# **관계 그래프**
+# pairplot( ) 그리기
+sns.pairplot(data=tips, hue='sex', diag_kind='hist', palette='husl')
+
+plt.subtitle
