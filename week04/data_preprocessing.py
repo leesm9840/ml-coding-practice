a83fdd58345864ac595c377ae3ad6c122765054b
leesm9840 <leesm9840@gmail.com>
2026-04-05 22:36:49 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -44,3 +44,6 @@ housing_num.head()
 imputer.fit(housing_num)
 
 print(imputer.statistics_)           # imputer 결과 값
+print(housing_num.median().values)   # 수동으로 계산한 중간값
+
+# 훈련 세트의 누락값을 imputer가 학습한 값으로 채우기
