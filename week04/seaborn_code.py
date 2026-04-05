9fb114b84f6b2bce204a4bd8f8d109c9e930f073
leesm9840 <leesm9840@gmail.com>
2026-04-05 22:21:49 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -9,11 +9,11 @@ print(tips.head())
 
 tips.info()
 
-# **������ ���� ������ �׷���**
+# **범주형 변수 산점도 그래프**
 
 import matplotlib.pyplot as plt
 
-# figure�� 2���� ���� �÷��� ����
+# figure에 2개의 서브 플롯을 생성
 fig = plt.figure(figsize=(15, 5))
 ax1 = fig.add_subplot(1, 2, 1)
 ax2 = fig.add_subplot(1, 2, 2)
