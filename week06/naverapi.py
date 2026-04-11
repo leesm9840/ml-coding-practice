46ec718bd270693982cded3b14e8ee3aa7708f72
leesm9840 <leesm9840@gmail.com>
2026-04-11 22:10:22 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -75,3 +75,5 @@ def getPostData(post, jsonResult, cnt):  #[CODE 3]
     org_link = post['originallink']
     link = post['link']
 
+    # %a: 짧은 형식의 요일 이름 (예, 'Mon', "Tue', "Wed', ... )
+    # %d: 일 (예, 01, 02, 03, ...)
