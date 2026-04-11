9d75ae3a46207a74412a9184b87f9e495514a3d0
leesm9840 <leesm9840@gmail.com>
2026-04-11 23:04:23 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -13,14 +13,14 @@ def main():
   result = []
 
   print("<< 국내 입국한 외국인의 통계 데이터를 수집합니다. >>")
-  nat_cd = input('국가 코드를 입력하세요(중국: 112 / 일본: 130 / �̱�: 275) :')
-  nStartYear = int(input('�����͸� �� ����� �����ұ��? : '))
-  nEndYear = int(input('�����͸� �� ����� �����ұ��? : '))
-  ed_cd = "E" 		                      # E : ���ѿܷ�������, D : �ؿ� �ⱹ
+  nat_cd = input('국가 코드를 입력하세요(중국: 112 / 일본: 130 / 미국: 275) :')
+  nStartYear = int(input('데이터를 몇 년부터 수집할까요? : '))
+  nEndYear = int(input('데이터를 몇 년까지 수집할까요? : '))
+  ed_cd = "E" 		                      # E : 방한외래관광객, D : 해외 출국
 
   jsonResult, result, natName, dataEND = getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear)  #[CODE 3]
 
-#�������� : csv ����
+  #파일저장 : csv 파일
   columns = ["�Ա��ڱ���", "�����ڵ�", "�Ա�����", "�Ա��� ��"]
   result_df = pd.DataFrame(result, columns = columns)
   result_df.to_csv('./%s_%s_%d_%s.csv' % (natName, ed_cd, nStartYear, dataEND), index = False, encoding = 'cp949')
