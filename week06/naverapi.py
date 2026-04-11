86ab10d558f6bfe5ed322c0de9d26fa1d7f03699
leesm9840 <leesm9840@gmail.com>
2026-04-11 21:57:52 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -27,3 +27,5 @@ def main():
         jsonResponse = getNaverSearch(node, srcText, start, 100) # [CODE 2]
 
     print('전체 검색 : %d 건' %total)
+
+    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding= 'utf8') as outfile:
