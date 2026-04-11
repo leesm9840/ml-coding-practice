a8133ce1aa8f9f2e29bb6f8efe68620e7e5c201e
leesm9840 <leesm9840@gmail.com>
2026-04-11 22:30:52 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -44,6 +44,7 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
         #jsonData를 출력하여 확인............................................
         print(json.dumps(jsonData, indent = 4, sort_keys = True, ensure_ascii = False))
 
+        natName = jsonData['response']['body']['items']['item']['natKorNm']
 
 
 
