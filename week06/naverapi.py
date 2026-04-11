26452ad54fe3729310ad1bc154fb54cbc6596603
leesm9840 <leesm9840@gmail.com>
2026-04-11 21:57:22 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -24,3 +24,6 @@ def main():
             getPostData(post, jsonResult, cnt)                # [CODE 3]
 
         start = jsonResponse['start'] + jsonResponse['display']
+        jsonResponse = getNaverSearch(node, srcText, start, 100) # [CODE 2]
+
+    print('전체 검색 : %d 건' %total)
