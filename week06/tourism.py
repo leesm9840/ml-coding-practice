1221d835508af0f697290c557656cb49c6b46d20
leesm9840 <leesm9840@gmail.com>
2026-04-11 23:01:23 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -4,7 +4,7 @@ import datetime
 import json
 import pandas as pd
 
-ServiceKey = "자신의 Service Key"
+ServiceKey = "rK764xacV_I4Fp86R7xP"
 
 """### [CODE 0]"""
 
@@ -59,7 +59,7 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
 
 def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
   service_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
-  parameters = "?_type=json&serviceKey=" + ServiceKey       # 인증키
+  parameters = "?_type=json&serviceKey=" + rK764xacV_I4Fp86R7xP       # 인증키
   parameters += "&YM=" + yyyymm
   parameters += "&NAT_CD=" + nat_cd
   parameters += "&ED_CD=" + ed_cd
