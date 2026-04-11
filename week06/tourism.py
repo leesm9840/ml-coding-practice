80d48fc53e447a73a464e95d7878747e021f7b6f
leesm9840 <leesm9840@gmail.com>
2026-04-11 22:36:53 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -58,6 +58,7 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
 """### [CODE 2]"""
 
 def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
+  service_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
 
 
 
