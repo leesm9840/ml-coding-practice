9bd4f5291404d70c4b19d89a72ffd69b3dc8b3cb
leesm9840 <leesm9840@gmail.com>
2026-04-11 22:34:52 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -51,6 +51,8 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
         print('[ %s_%s : %s ]' % (natName, yyyymm, num))
         print('------------------------------------------------------')
         jsonResult.append({'nat_name': natName, 'nat_cd': nat_cd, 'yyyymm': yyyymm, 'visit_cnt': num})
+        result.append([natName, nat_cd, yyyymm, num])
+
 
 
 
