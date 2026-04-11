71fbf7fb579719fb4805afd87bf0dff1f4c4cf7f
leesm9840 <leesm9840@gmail.com>
2026-04-11 22:14:22 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -86,7 +86,6 @@ def getPostData(post, jsonResult, cnt):  #[CODE 3]
         'title': title,
         'description': description,
         'org_link': org_link,
-
-
-
+        'link': link,
+       'pDate': pDate
     })
