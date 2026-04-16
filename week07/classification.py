0cabb0413775cd074a849cd801d1626b50fd66f9
leesm9840 <leesm9840@gmail.com>
2026-04-16 10:56:47 +0900

    Updated Changes

---
+++ week07/classification.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from sklearn.datasets import fetch_openml
+
+mnist = fetch_openml('mnist_784', as_frame=False)
+
+print(mnist.keys())  # data와 target만 사용
+
+X, y = mnist.data, mnist.target
+print(X)
+print(X.shape)       # 28 x 28 개의 픽셀 특징을 가진 이미지 70,000개
