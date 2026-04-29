e89d152952fad7b738821cd22d9db2fb5aa35d5a
leesm9840 <leesm9840@gmail.com>
2026-04-29 10:20:58 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -64,4 +64,9 @@ from sklearn.model_selection import cross_val_score
 
 y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 
-from sklearn.metrics import comfusion_matrix
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(y_train_5, y_train_pred)
+print(cm)
+
+y_train_perfect
