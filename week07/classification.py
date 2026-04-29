8ae58de5dfd800a6967cc8caad78bfb15a5150a2
leesm9840 <leesm9840@gmail.com>
2026-04-29 10:09:58 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -37,4 +37,6 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 
 # 이진 분류기 훈련
 y_train_5 = (y_train == '5')  # 5는 True고, 다른 숫자는 모두 False
-y_train_5 = (y)
+y_train_5 = (y_test == '5')
+
+from sklearn.linear_model import
