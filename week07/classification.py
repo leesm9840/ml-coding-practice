e9f5ac2e02020a42a7676ec6577f691d1b2af29d
leesm9840 <leesm9840@gmail.com>
2026-04-29 10:47:59 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -14,7 +14,7 @@ print(y.shape)
 import matplotlib.pyplot as plt
 
 def plot_digit(image_data):
-    image = image_data.resgape(28, 28)
+    image = image_data.reshape(28, 28)
     plt.imshow(image, cmap="binary")
     plt.axis("off")
 
