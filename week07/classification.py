7de3ae62d1b2138dbcbbe99c5ab11f4be5778a2f
leesm9840 <leesm9840@gmail.com>
2026-04-29 10:01:59 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -15,4 +15,8 @@ import matplotlib.pyplot as plt
 
 def plot_digit(image_data):
     image = image_data.resgape(28, 28)
-    plt.ima
+    plt.imshow(image, cmap="binary")
+    plt.axis("off")
+
+some_digit = X[0]
+plot_digit = X[0]
