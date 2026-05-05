6d2ae49611a5a777884d0b24639cbd9a870f677e
leesm9840 <leesm9840@gmail.com>
2026-05-05 13:15:49 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -133,3 +133,6 @@ genres
 plt.figure(figsize=(12, 6))
 
 sns.barplot(x=genres.values, y=genres.index, hue=genres.index, palette='RdGy')
+
+plt.title('Distribution of Genres for Movies and TV Shows on Netflix', fontsize=16)
+plt.xlabel('Count', fonts)
