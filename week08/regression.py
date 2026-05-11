80c9edc54b11116a72553133dc4d536d9e9bec5e
leesm9840 <leesm9840@gmail.com>
2026-05-11 18:34:08 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -73,3 +73,5 @@ X_new = np.linspace(-3, 3, 100).reshape(100, 1)
 X_new_poly = poly_features.transform(X_new)
 y_new = lin_reg.predict(X_new_poly)
 
+plt.figure(figsize=(6, 4))
+plt.plot(X, y, "b.")
