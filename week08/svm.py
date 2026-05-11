ae959a6db55bf9045ab6e5dadbf48fe2b5013243
leesm9840 <leesm9840@gmail.com>
2026-05-11 19:04:38 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -15,4 +15,8 @@ svm_clf = make_pipeline(StandardScaler(),
                         LinearSVC(C=1, dual=True, random_state=42))
 svm_clf.fit(X, y)
 
-X_new = 
+X_new = [[5.5, 1.7], [5.0, 1.5]]
+
+print(svm_clf.predict(X_new))
+print(svm_clf.decision_function(X_new))
+
