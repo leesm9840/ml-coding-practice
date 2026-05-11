046ac4d03cd81565d04b48933e9a5a9d525e8d8d
leesm9840 <leesm9840@gmail.com>
2026-05-11 19:09:38 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -44,3 +44,6 @@ polynomial_svm_clf.fit(X, y)
 
 # 가우스 RBF 커널
 rbf_kernel_svm_clf = make_pipeline(StandardScaler(),
+                                   SVC(kernel="rbf", gamma=5, C=0.001))
+rbf_kernel_svm_clf.fit(X, y)
+
