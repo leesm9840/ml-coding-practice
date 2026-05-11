d51fc9e1400b3203cc28c64a8e259467e80aea77
leesm9840 <leesm9840@gmail.com>
2026-05-11 18:19:13 +0900

    week08 regression practice

---
+++ week08/regression.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+
+plt.rc('font', size=14)
+plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=14)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+
+import numpy as np
+
+np.random.seed(42)                        # 코드 예제를 재현 가능하게 만들기 위해
+m = 100                                   # 샘플 개수
+X = 2* np.random.rand(m, 1)               # 열 벡터
+y = 4 + 3 * X + np.random.rand(m, 1)      # 열 벡터
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(6, 4))
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$")
+plt.ylabel("$y$", rotation=0)
+plt.axis([0, 2, 0, 15])
+plt.grid()
+plt.show()
+
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+lin_reg.intercept_, lin_reg.coef_
+
+X_new = np.array([[0], [2]])
+print(lin_reg.predict(X_new))
+
+# 확률적 경사 하강법
+
+from sklearn.linear_model import SGDRegressor
+
+sgd_reg = SGDRegressor(max_iter=1000, top=1e-5, penalty=None, eta0=0.01,
+                       n_iter_no_change=100, random_state=42)
+sgd_reg.fit(X, y.ravel())  # fit()이 1D 타깃을 기대하기 때문에 y.ravel()로 씁니다
+
+sgd_reg.intercept_, sgd_reg.coef_
+
+# 다항 회귀
+
+np.random.seed(42)
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 0.5 * X ** 2 + X + 2 + np.random.random(m, 1)
+
