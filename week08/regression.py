58d0b1059cfaa854d70e997cd53cadf388bd97c5
leesm9840 <leesm9840@gmail.com>
2026-05-11 18:25:38 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -26,4 +26,9 @@ plt.show()
 
 from sklearn.linear_model import LinearRegression
 
-lion_reg
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+lin_reg.intercept_, lin_reg.coef_
+
+X_new = np.array([[0], [2]])
+print(lin_reg.predict(X_new))
