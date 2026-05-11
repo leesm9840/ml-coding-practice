13d86117d4a5cdfe314dbdbad3f971ac80e4aca7
leesm9840 <leesm9840@gmail.com>
2026-05-11 18:23:38 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -17,3 +17,7 @@ y = 4 + 3 * X + np.random.randn(m, 1)     # 열 벡터
 import matplotlib.pyplot as plt
 
 plt.figure(figsize=(6, 4))
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$")
+plt.ylabel("$y$", rotation=0)
+plt.axis([0, 2, 0, 15])
