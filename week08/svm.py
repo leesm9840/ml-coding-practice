3f6b7cd6d11bde962fff9b1bc747d830cc88cb54
leesm9840 <leesm9840@gmail.com>
2026-05-11 19:02:08 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
-# 
+# 선형 SVM 분류
+
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+
+iris = load_iris(as_frame=True)
