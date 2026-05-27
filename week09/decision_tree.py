f1231cc3c5ad01aa41f4b01851a50354c504369c
leesm9840 <leesm9840@gmail.com>
2026-05-27 11:35:28 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -75,3 +75,6 @@ from sklearn.preprocessing import StandardScaler
 pca_pipeline = make_pipeline(StandardScaler(), PCA())
 X_iris_rotated = pca_pipeline.fit_transform(X_iris)
 tree_clf_pca = DecisionTreeClassifier(max_depth=2, random_state=42)
+tree_clf_pca.fit(X_iris_rotated, y_iris)
+
+# 추가 코드 - 이 셀은 그림 6-8을 생성하고 저장합니다.
