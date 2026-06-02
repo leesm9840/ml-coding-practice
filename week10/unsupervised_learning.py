1a93840839c4b6e46aa09391a8128a8703692124
leesm9840 <leesm9840@gmail.com>
2026-06-02 11:12:18 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -13,7 +13,7 @@ plt.rc('ytick', labelsize=10)
 # K-평균
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
-import numpym as np
+import numpy as np
 
 blob_centers = np.array([[ 0.2,  2.3], [-1.5 ,  2.3], [-2.8,  2.8],
                          [-2.8,  4.2], [-2.8,  1.0]])
