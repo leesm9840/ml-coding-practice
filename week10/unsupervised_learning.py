91e7a572829c4b5261c8e92fec816df61011f40d
leesm9840 <leesm9840@gmail.com>
2026-06-02 09:58:18 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -68,3 +68,7 @@ print(silhouette_score(X, kmeans.labels_))
 
 silhouette_scores = [silhouette_score(X, model.labels_) for model in kmeans_per_k[1:]]
 
+plt.figure(figsize=(8, 3))
+plt.plot(range(2, 10), silhouette_scores, "bo-")
+plt.xlabel("$k$")
+plt.ylabel
